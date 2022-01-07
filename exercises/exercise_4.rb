50fc0e45c7eaf6cc881d4292813d706186ec3fb1
stephkri <stephane.krims@hotmail.ca>
2022-01-07 03:58:38 +0000

    Finish exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+#surrey 224000 w
+#whistler 1900000 m
+#yaletown 430000 both
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "---MEN'S STORES---"
+@mens_stores.each { |store| puts "#{store.name}, #{store.annual_revenue}"}
+puts "---WOMEN'S STORES WITH LESS THAN $1M REVENUE"
+
+@womens_stores_small = Store.where(womens_apparel: true, annual_revenue: 0..1000000)
+@womens_stores_small.each { |store| puts "#{store.name}, #{store.annual_revenue}"}
