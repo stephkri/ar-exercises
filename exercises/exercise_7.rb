76f53d3e2a06994b76f435ae422f4b7bf155f8a4
stephkri <stephane.krims@hotmail.ca>
2022-01-07 05:15:43 +0000

    Finish exercise 7 core

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name."
+store_name = gets.chomp
+@store_attempt = Store.create(name: store_name)
+puts @store_attempt.errors[:annual_revenue]
